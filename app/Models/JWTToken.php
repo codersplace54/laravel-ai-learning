1c9583d07a8ebcbf7463df07198b4c3cc97e98eb
Binsi <binsi.bsp@gmail.com>
2025-06-30 19:01:02 +0530

    migrate_controllers_from_modules

---
+++ app/Models/JWTToken.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+
+class JWTToken extends Model
+{
+        use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'token',
+        'ip_address',
+        'user_agent',
+        'expires_at',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
