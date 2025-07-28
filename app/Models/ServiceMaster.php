52c66da591a8316a0e5a928a20c68ec90264ec10
Binsi <binsi.bsp@gmail.com>
2025-07-28 12:58:10 +0530

    service_master_table

--- app/Models/ServiceMaster.php
+++ app/Models/ServiceMaster.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class ServiceMaster extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'department_id',
+        'service_title_or_description',
+        'noc_name',
+        'noc_short_name',
+        'noc_type',
+        'noc_payment_type',
+        'target_days',
+        'has_input_form',
+        'depends_on_services',
+        'generate_id',
+        'generate_pdf',
+        'generated_id_format',
+        'label_noc_date',
+        'label_noc_doc',
+        'label_noc_no',
+        'label_valid_till',
+        // 'show_letter_date',
+        // 'show_letter_no',
+        'show_valid_till',
+        'auto_renewal',
+        'external_data_share',
+        'noc_validity',
+        'valid_for_upload',
+        'nsw_license_id',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
 }
