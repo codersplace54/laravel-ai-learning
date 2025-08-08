d8bd2ae1db75c864ea1e8abecc05a90b74ff447e
Binsi <binsi.bsp@gmail.com>
2025-08-08 11:54:19 +0530

    Create Service Fee Rule & CRUD

---
+++ app/Models/ServiceFeeRule.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceFeeRule extends Model
+{
+    protected $fillable = [
+        'id',
+        'service_id',
+        'renewal_cycle_id',
+        'fee_type',
+        'fixed_fee',
+        'question_id',
+        'condition_operator',
+        'condition_value_start',
+        'condition_value_end',
+        'fixed_calculated_fee',
+        'per_unit_fee',
+        'priority',
+        'status',
+        'created_at',
+        'updated_at'
+
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+}
